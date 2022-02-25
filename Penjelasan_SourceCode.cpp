03788468f6f918fcc65d55c8503387fbee3c0bc1
G1A021006MUHAMMADARYABINTANGPRATAMA <99008420+G1A021006MUHAMMADARYABINTANGPRATAMA@users.noreply.github.com>
2022-02-25 22:06:28 +0700

    Add files via upload

---
+++ Penjelasan_SourceCode.cpp
@@ -0,0 +1,19 @@
+Nama : MUHAMMAD ARYA BINTANG PRATAMA
+NPM : G1A021006
+Kelas : Informatika-B
+Struktur Data dan Algoritma
+
+Cara kerja dari program yang saya buat adalah untuk mencari luas dan keliling Jajaran Genjang,dengan cara memasukkan input yang diminta kepada pengguna yaitu berupa panjang alas,tinggi, dan panjang sisi miring. Lalu output akan menghasilkan Besar luas dan keliling jajaran genjang dari input yang telah dimasukkan.
+
+- #include berfungsi untuk mengimpor fungsi-fungsi yang sudah didefinisikan pada header file.Header file adalah file yang berisi definisi fungsi yang sudah dibuat. Tujuannya agar bisa digunakan pada program C yang lainnya. File ini berekstensi .h, contoh: <stdio.h>.;<conio.h>
+- Struct merupakan keyword yang memungkinkan membuat deklarasi yang di dalamnya dapat terdapat banyak variabel, hasil deklarasi ini akan menghasilkan user-defined data type yaitu tipe structure yang berguna untuk mendirikan object.
+- Float adalah salah satu tipe data Numerik yang memungkinkan untuk menyimpan nilai dalam memori bersifat bilangan pecahan atau real, maupun eksponensial.
+- Void adalah fungsi yang tidak mengembalikan nilai keluaran ( return output ) yang didapat dari hasil proses tersebut, ini kenapa fungsi ini disebut void,  secara harfiah berarti kosong.
+- Fungis main() adalah fungsi utama dalam program,fungsi ini akan dieksekusi pertamakali saat program dijalankan.
+- Printf digunakan untuk membuat sebuah output berupa tampilan.
+- %f berfungsi untuk mengeksekusi type data berbentuk pecahan.
+- Return berfungsi untuk mengakhri eksekusi dari function tersebut, dan return juga dapat memberikan nilai pada saat akhir dari function kepada pemanggil.
+-  \n untuk memberi space pada output.
+- Fungsi scanf() adalah fungsi untuk mengambil input dari keyboard. Fungsi ini memiliki format seperti fungsi printf() . Format yang diberikan tergantung dari jenis tipe data apa yang ingin kita ambil.
+- getch () : merupakan singakatan dari get character and echo yang digunakan untuk menahan output suatu program dan akan kembali mengeksekusi setelah kita melakukan inputan baik tombol enter atau lainnya dan inputan tersebut tidak ditampilkan dalam windows.
+- operator * dan + berfungsi untuk mengalikan dan menambahkan angka/inputan berupa angka pada source code.
